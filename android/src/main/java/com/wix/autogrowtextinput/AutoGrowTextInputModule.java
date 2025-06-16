23329e6ff14a66792b119b944562590e46549c94
Paul Kwak <pkwak@khoros.com>
2025-06-16 10:24:35 -0700

    remove setBlurOnSubmit. default behavior on multiple should be same.

--- android/src/main/java/com/wix/autogrowtextinput/AutoGrowTextInputModule.java
+++ android/src/main/java/com/wix/autogrowtextinput/AutoGrowTextInputModule.java
@@ -52,7 +52,6 @@ public class AutoGrowTextInputModule extends ReactContextBaseJavaModule {
                 if (param.hasKey("maxHeight") && !param.isNull("maxHeight")) {
                     mMaxHeight = dpToPx(param.getDouble("maxHeight"));
                 }
-                editText.setBlurOnSubmit(false);
                 editText.setOnTouchListener(new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View v, MotionEvent event) {
